7ed6dd0905b1f117c96802dc4f1497b53d5815cc
ktodorov <kztodorov@outlook.com>
2019-04-06 14:58:18 +0200

    Changed visualization from networkx to graphviz

--- qualitative-reasoning/utils.py
+++ qualitative-reasoning/utils.py
@@ -1,4 +1,4 @@
-import networkx as nx
+from graphviz import Digraph
 import matplotlib.pyplot as plt
 from typing import List
 
@@ -21,41 +21,33 @@ def create_representation_graph(
         node_size: int = 500,
         file_path: str = None):
 
-    G = nx.DiGraph()
-    # add all quantities
-    # G.add_nodes_from([quantity.label for quantity in quantities])
-    G.add_nodes_from(states)
+    G = Digraph(comment='The Qualitative Model')
 
-    # node_color_map = [quantity.color for quantity in quantities]
+    for state in states:
+        G.node(state)
 
-    # add the edges between the quantities
-    # also populate the node_color_map
     for edge in edges:
-        G.add_edge(edge[0], edge[1])
-        # for quantity_connection in quantity.connections:
-        #     G.add_edge(quantity.label, quantity_connection)
+        G.edge(edge[0], edge[1])
 
-        # quantity_color_map.append(quantity.color)
+    # nx.draw(
+    #     G,
+    #     font_size=font_size,
+    #     font_color=font_color,
+    #     # node_color=node_color_map,
+    #     with_labels=True,
+    #     node_size=node_size)
 
-    nx.draw(
-        G,
-        font_size=font_size,
-        font_color=font_color,
-        # node_color=node_color_map,
-        with_labels=True,
-        node_size=node_size)
-
-    if file_path:
-        plt.savefig(file_path)
-    else:
-        plt.show()
+    G.render('test-output/round-table.gv', view=True)
+    # if file_path:
+    #     plt.savefig(file_path)
+    # else:
+    #     plt.show()
 
 
 def get_quantity_states(quantity: Quantity) -> List[str]:
     quantity_states = []
     for quantity_space in quantity.spaces:
         for quantity_gradient in quantity.gradients:
-            # quantity_states.append(f'{quantity.label}_{quantity_space.label}')
             quantity_states.append((quantity, quantity_space, quantity_gradient))
 
     return quantity_states
