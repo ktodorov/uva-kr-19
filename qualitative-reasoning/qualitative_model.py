4122701a9e7a67d434fd2e631051d9de1f8bda4d
ktodorov <kztodorov@outlook.com>
2019-04-06 14:57:47 +0200

    Implemented initial transitions calculation

--- qualitative-reasoning/qualitative_model.py
+++ qualitative-reasoning/qualitative_model.py
@@ -3,6 +3,7 @@ from typing import List
 from quantity import Quantity
 from quantity_dependency import QuantityDependency
 from qualitative_state import QualitativeState
+from quantity_state import QuantityState
 from enums.dependency_type import DependencyType
 import utils
 import itertools
@@ -70,7 +71,7 @@ class QualitativeModel:
     def visualize_states(self):
         all_combinations = self.generate_all_combinations()
 
-        self.print_combinations(all_combinations)
+        # self.print_combinations(all_combinations)
 
         nodes = []
         edges = []
@@ -86,13 +87,15 @@ class QualitativeModel:
                 if self.transition_exists(start_state, end_state):
                     end_string = utils.get_state_string(end_state)
                     edges.append((start_string, end_string))
+                
+                # return
 
         # print(len(edges))
-        # utils.create_representation_graph(
-        #     nodes,
-        #     edges,
-        #     font_size=5,
-        #     node_size=1500)
+        utils.create_representation_graph(
+            nodes,
+            edges,
+            font_size=10,
+            node_size=1500)
 
     # Constraint check
 
@@ -269,19 +272,32 @@ class QualitativeModel:
 
         return True
 
+    # def qualitative_states_are_equal(self, state1, state2):
+    #     for i, quantity in enumerate(state1.get_quantities()):
+    #         if (state1.qualitative_state_quantities[i].quantity.label != state2.qualitative_state_quantities[i].quantity.label
+    #                 or state1.qualitative_state_quantities[i].value.label != state2.qualitative_state_quantities[i].value.label
+    #                 or state1.qualitative_state_quantities[i].gradient != state2.qualitative_state_quantities[i].gradient):
+    #             return False
+
+    #     return True
+
     def transition_exists(self, start_state: QualitativeState, end_state: QualitativeState):
-        end_state_quantities = end_state.get_quantities()
+        possible_changes = self.get_possible_changes(start_state)
+        if end_state not in possible_changes:
+            return False
+
         # If we have quantity which is not constrained -
         # it cannot change with more than two steps in one transition
-        for i, start_quantity_state in enumerate(start_state.get_quantities()):
-            if not self.constraint_dependencies[start_quantity_state.quantity.label]:
-                # if ((start_quantity_state.value.label == '0' and end_state_quantities[i].value.label == 'max') or
-                #         start_quantity_state.value.label == 'max' and end_state_quantities[i].value.label == '0'):
+        # for i, start_quantity_state in enumerate(start_state.get_quantities()):
+        #     if not self.constraint_dependencies[start_quantity_state.quantity.label]:
+        #         # if ((start_quantity_state.value.label == '0' and end_state_quantities[i].value.label == 'max') or
+        #         #         start_quantity_state.value.label == 'max' and end_state_quantities[i].value.label == '0'):
 
-                #     return False
-                if self.amount_of_changes(start_state, end_state, i) > 1:
-                    return False
-            # else:
+        #         #     return False
+        #         if self.amount_of_changes(start_state, end_state, i) > 1:
+        #             return False
+        #     else:
+        #         possible_change_states = self.get_possible_changes(start_quantity_state)
                 # if self.amount_of_changes(start_state, end_state) > 1:
                     # if we have constraint, we check if the old value is not the constrained and the new independent is the one constraining
                     # for independent_quantity_label, dependency_tuples in self.constraint_dependencies[start_quantity_state.quantity.label].items():
@@ -296,13 +312,61 @@ class QualitativeModel:
                         # if old_value == new_value or new_value not in start_dependecy_labels:
                         #     return False
 
-        if (start_quantity_state.value.label != end_state_quantities[i].value.label and start_quantity_state.gradient == '0'):
-            if not self.incoming_dependencies[start_quantity_state.quantity.label]:
-                return False
-            else:
-                pass  # ????
+        # if (start_quantity_state.value.label != end_state_quantities[i].value.label and start_quantity_state.gradient == '0'):
+        #     if not self.incoming_dependencies[start_quantity_state.quantity.label]:
+        #         return False
+        #     else:
+        #         pass  # ????
 
         return True
+
+    def get_possible_changes(self, state: QualitativeState) -> List[QualitativeState]:
+        possible_states_by_quantity = []
+        all_gradients_are_zero = True
+
+        for i, quantity_state in enumerate(state.get_quantities()):
+            possible_states_by_quantity.append([])
+            possible_states_by_quantity[i].append(quantity_state)
+            quantity_state_value_index = quantity_state.quantity.spaces.index(quantity_state.value)
+            # if our gradient is + and we can move further up the values
+            if quantity_state.gradient == '+' and quantity_state_value_index != len(quantity_state.quantity.spaces) - 1:
+                all_gradients_are_zero = False
+                new_gradient = quantity_state.gradient
+                # if we have reached the end, the gradient must become neutral
+                if quantity_state_value_index == len(quantity_state.quantity.spaces) - 2:
+                    new_gradient = '0'
+
+                new_value = quantity_state.quantity.spaces[quantity_state_value_index + 1]
+                new_possible_state = QuantityState(quantity_state.quantity, new_value, new_gradient)
+                possible_states_by_quantity[i].append(new_possible_state)
+            # if our gradient is - and we can move further down the values
+            elif quantity_state.gradient == '-' and quantity_state_value_index != 0:
+                all_gradients_are_zero = False
+                new_gradient = quantity_state.gradient
+                # if we have reached the end, the gradient must become neutral
+                if quantity_state_value_index == 1:
+                    new_gradient = '0'
+
+                new_value = quantity_state.quantity.spaces[quantity_state_value_index - 1]
+                new_possible_state = QuantityState(quantity_state.quantity, new_value, new_gradient)
+                possible_states_by_quantity[i].append(new_possible_state)
+
+        all_possible_combinations = list(itertools.product(*possible_states_by_quantity))
+        
+        # we remove the combination where the state stays the same
+        if not all_gradients_are_zero:
+            all_possible_combinations.pop(0)
+
+        possible_states = []
+        for possible_combination in all_possible_combinations:
+            current_state = QualitativeState()
+            for quantity_state in possible_combination:
+                current_state.add_quantity_state(quantity_state)
+            
+            possible_states.append(current_state)
+        
+        return possible_states
+
 
     def amount_of_changes(self, state1: QualitativeState, state2: QualitativeState, index: int = None) -> int:
         result = 0
@@ -329,10 +393,4 @@ class QualitativeModel:
         print('---------------------------------------------')
 
         for qualitative_state in all_states:
-            print('(', end='')
-
-            for quantity in qualitative_state.get_quantities():
-                print(
-                    f'({quantity.quantity.label}, {quantity.value.label}, {quantity.gradient}), ', end='')
-
-            print(')')
+            print(str(qualitative_state))
--- qualitative-reasoning/qualitative_state.py
+++ qualitative-reasoning/qualitative_state.py
@@ -4,12 +4,16 @@ from quantity import Quantity
 from quantity_space import QuantitySpace
 from quantity_state import QuantityState
 
+
 class QualitativeState:
     def __init__(self):
         self.qualitative_state_quantities = []
 
     def add_quantity(self, quantity: Quantity, value: QuantitySpace, gradient: str):
         quantity_state = QuantityState(quantity, value, gradient)
+        self.qualitative_state_quantities.append(quantity_state)
+
+    def add_quantity_state(self, quantity_state: QuantityState):
         self.qualitative_state_quantities.append(quantity_state)
 
     def get_quantities(self) -> List[QuantityState]:
@@ -19,3 +23,26 @@ class QualitativeState:
         for quantity_state in self.qualitative_state_quantities:
             if quantity_state.quantity.label == quantity_label:
                 return quantity_state
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if not isinstance(other, QualitativeState):
+            return False
+
+        for i, quantity in enumerate(self.get_quantities()):
+            if (self.qualitative_state_quantities[i].quantity.label != other.qualitative_state_quantities[i].quantity.label
+                    or self.qualitative_state_quantities[i].value.label != other.qualitative_state_quantities[i].value.label
+                    or self.qualitative_state_quantities[i].gradient != other.qualitative_state_quantities[i].gradient):
+                return False
+
+        return True
+
+    def __str__(self):
+        result = '('
+
+        for quantity in self.get_quantities():
+            result = result + \
+                f'({quantity.quantity.label}, {quantity.value.label}, {quantity.gradient}), '
+
+        result = result + ')'
+        return result
