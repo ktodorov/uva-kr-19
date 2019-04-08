7d5429873c87719dd6bdd22b5369165dc8a9c214
ktodorov <kztodorov@outlook.com>
2019-04-08 20:44:21 +0200

    Improved dependency generalization with gradients

--- qualitative-reasoning/quantity_dependency.py
+++ qualitative-reasoning/quantity_dependency.py
@@ -1,16 +1,18 @@
 from enums.dependency_type import DependencyType
-# from quantity import Quantity
-
 
 class QuantityDependency:
     def __init__(self, dependency_type: DependencyType,
                  start_quantity,
                  end_quantity,
-                 start_quantity_space=None,
-                 end_quantity_space=None):
+                 start_quantity_values=None,
+                 start_quantity_gradients=None,
+                 end_quantity_values=None,
+                 end_quantity_gradients=None):
 
         self.type = dependency_type
         self.start = start_quantity
         self.end = end_quantity
-        self.start_space = start_quantity_space
-        self.end_space = end_quantity_space
+        self.start_quantity_values = start_quantity_values
+        self.start_quantity_gradients = start_quantity_gradients
+        self.end_quantity_values = end_quantity_values
+        self.end_quantity_gradients = end_quantity_gradients
